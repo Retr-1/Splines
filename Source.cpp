6afd3a99ced0b89d2a092819662bcffcb49fe29b
Chuck Norris <me@example.com>
2024-08-07 23:52:13 +0200

    added points and dragging

--- Source.cpp
+++ Source.cpp
@@ -2,21 +2,44 @@
 #include "olcPixelGameEngine.h"
 
 class Spline {
+public:
 	std::vector<olc::vi2d> points;
 
-public:
-	olc::vi2d getPointByT(float t) {
-		
+	olc::vi2d getPointByT(float p) {
+
+		/*cubic e0 = { -1,2,-1 };
+		cubic e1 = { 3,-5,2 };
+		cubic e2 = { -3, 4, 1 };
+		cubic e3 = { 1,-1,0 };*/
+		int i = (int)p;
+		float t = p - i;
+
+		float tt = t * t;
+		float ttt = tt * t;
+
+		float v0 = -ttt + 2 * tt - t;
+		float v1 = 3 * ttt - 5 * tt + 2 * t;
+		float v2 = -3 * ttt + 4 * tt + t;
+		float v3 = ttt - tt;
+
+		return points[i] * v0 + points[i + 1] * v1 + points[i + 2] * v2 + points[i + 3] * v3;
 	}
 };
 
 // Override base class with your custom functionality
 class Example : public olc::PixelGameEngine
 {
-	Track track;
+	Spline spline;
+	int selected = -1;
+
+	void drawPoints(const std::vector<olc::vi2d>& points) {
+		for (int i = 0; i< spline.points.size(); i++) {
+			FillCircle(spline.points[i], 10, i==selected ? olc::YELLOW : olc::GREY);
+		}
+	}
 
 public:
-	Example() : 
+	Example()
 	{
 		// Name your application
 		sAppName = "Example";
@@ -26,20 +49,47 @@ public:
 	bool OnUserCreate() override
 	{
 		// Called once at the start, so create things here
+		int x = 100;
+		for (int i = 0; i < 10; i++) {
+			spline.points.push_back(olc::vi2d(x, 300));
+			x += 50;
+		}
 		return true;
 		
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		// Called once per frame, draws random coloured pixels
+		if (GetMouse(olc::Mouse::LEFT).bPressed) {
+			for (int i = 0; i < spline.points.size(); i++) {
+				auto p = spline.points[i];
+				auto m = GetMousePos();
+				float d = sqrtf(powf(m.x - p.x, 2) + powf(m.y - p.y, 2));
+				if (d < 10)
+					selected = i;
+			}
+		}
+
+		if (GetMouse(olc::Mouse::LEFT).bHeld && selected != -1) {
+			spline.points[selected] = GetMousePos();
+		}
+
+		if (GetMouse(olc::Mouse::LEFT).bReleased) {
+			selected = -1;
+		}
+
+
+		Clear(olc::BLACK);
+		drawPoints(spline.points);
+
+		return true;
 	}
 };
 
 int main()
 {
 	Example win;
-	if (win.Construct(256, 240, 4, 4))
+	if (win.Construct(800, 800, 1, 1))
 		win.Start();
 	return 0;
 }
